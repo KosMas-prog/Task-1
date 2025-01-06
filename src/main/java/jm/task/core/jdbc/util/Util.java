5a8b3174226e58970e76cce362499afb07935a3a
Konstantin Shkilev <shkiliov@mail.ru>
2025-01-06 13:02:42 +0500

    Выполнение задания 1.1.3

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:postgresql://localhost:5432/Task1";
+    private static final String USERNAME = "postgres";
+    private static  final  String PASSWORD = "sakura";
+
+    public static Connection getConnection(){
+        Connection connection = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Connection to Task1 DB succeeded!");
+        } catch (SQLException  | ClassNotFoundException e) {
+            System.out.println("Connection failed...");
+            e.printStackTrace();
+        }
+        return connection;
+    }
+
+    public static void closeConnection(Connection connection) {
+        if (connection!=null) {
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
