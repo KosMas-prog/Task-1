46544856db9cf6a725fdb6a5318850a6ea835aba
Konstantin Shkilev <shkiliov@mail.ru>
2025-01-23 16:52:28 +0500

    Решение задачи 1.1.4 (исправил запрос на диалект PostgreSQL при создании таблицы)

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,42 +1,97 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+import java.util.ArrayList;
 
+import javax.persistence.Entity;
 import java.util.List;
+import java.util.concurrent.Callable;
 
+@Entity
 public class UserDaoHibernateImpl implements UserDao {
-    public UserDaoHibernateImpl() {
 
+    private final SessionFactory sessionFactory;
+
+    public UserDaoHibernateImpl() {
+        Configuration configuration = new Configuration().addAnnotatedClass(User.class);
+        this.sessionFactory = configuration.buildSessionFactory();
     }
 
 
     @Override
     public void createUsersTable() {
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
 
+            String sql = "CREATE TABLE IF NOT EXISTS users" +
+                    "(id SERIAL PRIMARY KEY, " +
+                    "name VARCHAR(50) NOT NULL, " +
+                    "lastName VARCHAR(50) NOT NULL, " +
+                    "age SMALLINT NOT NULL)";
+
+            session.createNativeQuery(sql).executeUpdate();
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
 
+            String sql = "DROP TABLE IF EXISTS users";
+
+            session.createNativeQuery(sql).executeUpdate();
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
+        try (Session session = sessionFactory.getCurrentSession()){
+            session.beginTransaction();
 
+            session.save(new User(name, lastName, age));
+
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
 
+            User user = session.get(User.class, id);
+            if (user != null) {
+                session.delete(user);
+            }
+            session.getTransaction().commit();;
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-        return null;
+        try (Session session = sessionFactory.openSession()){
+            return session.createQuery("FROM User", User.class).list();
+        }
     }
 
     @Override
     public void cleanUsersTable() {
+        try (Session session = sessionFactory.openSession()){
+            session.beginTransaction();
 
+            Query query = session.createQuery(("DELETE FROM User"));
+            query.executeUpdate();
+
+            session.getTransaction().commit();;
+        }
     }
 }
